cf3bd460a46e4619371167b97f764e77932a0a6f
PantheraDev <rojaschristopher1@gmail.com>
2025-05-17 16:44:14 -0400

    feat: initialize frontend project with React, Vite, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
