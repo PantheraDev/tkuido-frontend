f3a5ed1fc43e536f7542c2b28b52ee55a438c769
YC96 <yoikerth0@gmail.com>
2026-01-18 11:05:09 -0400

    Corregir la importación del componente Home en App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "../pages/MainPage";
+import MainPage from "../pages/MainPage";
 import Login from "../pages/LoginPage";
 import Register from "../pages/SignUpPage";
 import UserProfilePage from "./UserProfilePage";
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/tkuido-frontend" element={<Home />} />
+        <Route path="/tkuido-frontend" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} />
         <Route path="/payment" element={<PayMent />} />
