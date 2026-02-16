057a53d45f2200e7d97ab7de88b848ded30c4220
YC96 <yoikerth0@gmail.com>
2026-02-16 10:20:37 -0400

    feat: reemplazar StrictMode por AuthProvider en el renderizado principal

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./pages/App.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 const rootElement = document.getElementById("root");
 
@@ -10,7 +10,7 @@ if (!rootElement) {
 }
 
 createRoot(rootElement).render(
-  <StrictMode>
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>,
 );
